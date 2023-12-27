902a9eb32973dc63eeae08c1ea063113f8cf6df7
JamaalKohler <koeagwydvi@outlook.com>
2023-12-28 00:30:50 +0800

    aut

---
+++ src/js/polyfills/index.js
@@ -0,0 +1,25 @@
+// Order is important!
+
+import './global.js';
+import './timers.js';
+import './dom-exception.js';
+import './event-target-polyfill.js';
+import './url-polyfill.js';
+
+import './xhr.js';
+import 'whatwg-fetch';
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
+
+import 'web-streams-polyfill/es2018';
+
+import './text-encoding.js';
+
+import './blob.js';
+import './file.js';
+import './console.js';
+import './crypto.js';
+import './performance.js';
+import './storage.js';
+import './wasm.js';
+import './worker.js';
+import './ws.js';
